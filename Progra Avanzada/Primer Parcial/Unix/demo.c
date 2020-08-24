5c5fdbabdd30e409b49d514824572a5486a3f84f
danielroa98 <daniel.roa98@gmail.com>
2020-08-24 08:56:41 -0500

    Inicio 3era semana

---
+++ Progra Avanzada/Primer Parcial/Unix/demo.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct Libros
+{
+    char * nombre;
+    int paginas;
+}libros;
+
+int main(int argc, char const *argv[])
+{
+    libros * librero;
+
+    
+
+    return 0;
+}
